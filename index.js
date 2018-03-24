081b6b03f20496a236336fc6fc7e8b72c4919182
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-24 15:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution here:
-function driversWithRevenueOver() {
-
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function checkRevenue(revenue) {
+    driver.revenue >= revenue
+  }) 
 };
 
 function driverNamesWithRevenueOver() {
