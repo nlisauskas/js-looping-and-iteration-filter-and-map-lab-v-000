0ffb2a1bb31d0ec8978ab8dcccb523015c8264f1
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-24 16:08:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ function driverNamesWithRevenueOver(drivers, revenue) {
   })
 };
 
-function exactMatch() {
+function exactMatch(drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
 
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
+
+    return matches;
+  })
 };
 
 function exactMatchToList() {
